40679647c4eeff5ba3040e549dd214ccb649b655
Roger Medellin <medr33@yahoo.com>
2023-04-14 14:36:54 -0400

    added new questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,38 +4,79 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
+  {
+    type: 'input',
+    message: 'What is your GitHub username?',
+    name: 'Title',
+   },  
+  
+   {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'Title',
+   },
+  
+   {
+    type: 'input',
+    message: 'What is your project name?',
+    name: 'Title',
+   },
+  
+  
+  {
       type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
+      message: 'Please write a short description of your project?',
+      name: 'Title',
      },
      {
-         type: 'password', 
-         message: 'What is your password?',
-         name: 'password',
+         type: 'input', 
+         message: 'What kind of license should your project have?',
+         name: 'Description',
      },
-     {
-//     {
+     {   type: 'input', 
+         message: 'What command should be run to install dependencies?',
+         name: 'Table of Contents', }
+];
+     
 // //      type: 'list',  input
 //       message: 'What is your preferred method of communication?',
 //       name: 'contact',
 //       choices: ['email', 'phone', 'telekinesis'],
-     },
-     ];
+     
+     
 
-// TODO: Create a function to write README file
+// TODO: Create a function to write README 
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path)
+}
 
 
-.then((answers) => {
-    const question = generateReadMe(answers);
+// .then((answers) => {
+//     const question = generateReadMe(answers);
 
-    fs.writeFile('generateReadMed', question, (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
-});
+//     fs.writeFile('readMe.md', question, (err) =>
+//       err ? console.log(err) : console.log('Success!')
+//     );
+// });
 
-// TODO: Create a function to initialize app
-function init() {}
+// TODO: Create a function to initialize 
+
+// function init() {}
+
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+      console.log('Generating README...');
+      writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+    });
+  }
+
+
+
+
+
+
+
+
 
 // Function call to initialize app
 init();
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# generator-readme
+# generator-
+https://github.com/Rogerrox/generator-readme
+# Discription
+My application creates a professional README.md file that is efficient and seamless to use. 
+# Table of Contents
+*[Installaton](#installation)
+*[Usage](#usage)
+*[License](#license)
+*[Contribution](#contribution)
+*[Test](#test)
+*[Questions](#questions)
+#Installation
+// These dependencies listed must be installed for this application to work: 
